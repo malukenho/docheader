866fbb87bc2d9b3dda326f691553ade6e36a8e3b
Jefersson Nathan <admin@phpse.net>
2016-06-07 15:27:24 -0300

    add travis build config

---
+++ .travis.coverage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -x
+if [ "$TRAVIS_PHP_VERSION" = '7.0' ] ; then
+    ./vendor/bin/phpunit --disallow-test-output --report-useless-tests --coverage-clover ./clover.xml
+    wget https://scrutinizer-ci.com/ocular.phar
+    php ocular.phar code-coverage:upload --format=php-clover ./clover.xml
+fi

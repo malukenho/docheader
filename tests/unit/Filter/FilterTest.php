2eb383823f3e3b00918048ef38cababd31a1eedd
Jefersson Nathan <admin@phpse.net>
2016-11-15 01:11:45 -0300

    Added tests for \DocHeader\Filter\Filter

---
+++ tests/unit/Filter/FilterTest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+namespace DocHeaderTest\Filter;
+use DocHeader\Filter\Filter;
+
+/**
+ * Tests for {@see \DocHeader\Filter\Filter}.
+ *
+ * @group   Unitary
+ * @author  Jefersson Nathan <malukenho@phpse.net>
+ * @license MIT
+ *
+ * @covers  \DocHeader\Filter\Filter
+ */
+final class FilterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_have_replace_current_year_placeholder_as_default_filter()
+    {
+        $this->assertClassHasAttribute('dockBlockDefaultFilters', Filter::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_apply_default_filters_to_given_docheader()
+    {
+        $docBlock = 'Year %year%';
+        $filter = new Filter($docBlock);
+
+        $this->assertSame('Year ' . date('Y'), $filter->applyFilters());
+    }
+}

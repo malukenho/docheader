4a4f23bf1394c5970eb4d2ae5d2b40624f68d67a
Jefersson Nathan <admin@phpse.net>
2016-11-15 01:24:25 -0300

    Allow code coverage on 5.6

--- .travis.coverage.sh
+++ .travis.coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-if [ "$TRAVIS_PHP_VERSION" = '7.0' ] ; then
+if [ "$TRAVIS_PHP_VERSION" = '5.6' ] ; then
     ./vendor/bin/phpunit --disallow-test-output --report-useless-tests --coverage-clover ./clover.xml
     wget https://scrutinizer-ci.com/ocular.phar
     php ocular.phar code-coverage:upload --format=php-clover ./clover.xml
